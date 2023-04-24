d12b4aa3e2389110a40c261325817e3987a8bd1d
Kotec <vivalastelmah@gmail.com>
2023-04-24 17:39:28 +0300

    fix(extended-2.1, basic-3.2, basic-3.3): complete bem naming, fix css selectors and js accordingly to new naming

--- app.js
+++ app.js
@@ -41,10 +41,11 @@ var createNewTaskElement=function(taskString){
     editInput.className="input--task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button--edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button--delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.classList.add('button--delete__img')
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -84,8 +85,8 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("todo-current__item--edit");
+    var editBtn=listItem.querySelector(".button--edit");
+    var containsClass=listItem.classList.contains("items-list__item-save");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +100,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("todo-current__item--edit");
+    listItem.classList.toggle("items-list__item-save");
 };
 
 
@@ -156,8 +157,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".button--edit");
+    var deleteButton=taskListItem.querySelector(".button--delete");
 
 
     //Bind editTask to edit button.
